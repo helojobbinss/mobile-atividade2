7a589662d7c44510ec4d5de5399e1cd9d385d091
Coldext <willc2018@outlook.com>
2026-04-12 22:57:29 -0300

    Feat: Commit para Atualizar Livros - Willian Galvão

--- bin/livro.dart
+++ bin/livro.dart
@@ -6,4 +6,16 @@ class Livro {
 
   Livro(this.id, this.titulo, this.anoPublicacao, this.autor);
 
+void atualizar(String titulo, int anoPublicacao, String autor) {
+    this.titulo = titulo;
+    this.anoPublicacao = anoPublicacao;
+    this.autor = autor;
+  }
+
+  void exibir() {
+    print('ID: $id, Título: $titulo, Ano: $anoPublicacao, Autor: $autor');
+  }
+
+
+
 }
