07c18b69706a1474ef5f0aa0e4e955ba5c244540
Rafakseixas <Rafabrseixas@gmail.com>
2026-04-12 21:00:10 -0300

    feat:listar-remover-atualizar:livros

--- bin/biblioteca.dart
+++ bin/biblioteca.dart
@@ -8,5 +8,29 @@ class Biblioteca {
     Livro novoLivro = Livro(id, titulo, anoPublicacao, autor);
     livros.add(novoLivro);
   }
+void listarLivros() {
+    if (livros.isEmpty) {
+      print('Nenhum livro cadastrado.');
+    } else {
+      print('Livros cadastrados:');
+      for (var livro in livros) {
+        livro.exibir();
+      }
+    }
+  }
+
+  void removerLivro(int id) {
+    livros.removeWhere((livro) => livro.id == id);
+  }
+
+  void atualizarLivro(int id, String titulo, int anoPublicacao, String autor) {
+    for (var livro in livros) {
+      if (livro.id == id) {
+        livro.atualizar(titulo, anoPublicacao, autor);
+        return;
+      }
+    }
+    print('Livro não encontrado.');
+  }
 
 }
